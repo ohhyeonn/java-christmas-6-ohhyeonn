e06182f3c83f98b551b87891626c3799eff96d60
ohhyeonn <ohhyeon120@gmail.com>
2023-11-10 03:51:25 +0900

    feat : add feature 개수 입력 숫자인지 검증
    edit InputVIew.java

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
     private static final String ENTER_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ENTER_MENUS = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private static final String ERROR_NOT_VALID_MENUS = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
-
+    private static final String ERROR_COUNT_HAS_TO_BE_NUMBER = "[ERROR] 메뉴 개수는 숫자 여야 합니다.";
     public static Integer readDate() {
         int date;
         while (true) {
@@ -46,6 +46,8 @@ public class InputView {
                 printMenusInstruction();
                 String[] menus = getMenus();
                 break;
+            } catch (NumberFormatException e) {
+                System.out.println(ERROR_COUNT_HAS_TO_BE_NUMBER);
             } catch (IllegalArgumentException e) {
                 System.out.println(ERROR_NOT_VALID_MENUS);
             }
