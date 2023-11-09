7e4d038d9054c97aef39d2a70993f32c827cd9d6
ohhyeonn <ohhyeon120@gmail.com>
2023-11-10 03:46:04 +0900

    feat : add feature 메뉴와 개수 잘못 입력시 재입력
    edit InputVIew.java

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -7,6 +7,8 @@ public class InputView {
     private static final String ERROR_VISIT_DATE_IS_NUMBER = "[ERROR] 방문일자는 숫자여야 합니다.";
     private static final String ENTER_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ENTER_MENUS = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static final String ERROR_NOT_VALID_MENUS = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
     public static Integer readDate() {
         int date;
         while (true) {
@@ -39,8 +41,15 @@ public class InputView {
     }
 
     public static void readMenu() {
-        printMenusInstruction();
-        String[] menus = getMenus();
+        while (true) {
+            try {
+                printMenusInstruction();
+                String[] menus = getMenus();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR_NOT_VALID_MENUS);
+            }
+        }
     }
 
     private static void printMenusInstruction() {
