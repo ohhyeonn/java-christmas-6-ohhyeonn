56c7b492e85b454ff491e7cdb413e296bb165462
ohhyeonn <ohhyeon120@gmail.com>
2023-11-10 01:28:27 +0900

    feat : add feature 인사말 출력
    add Controller , add OutputView , edit Application

---
+++ src/main/java/christmas/Controller/Controller.java
@@ -0,0 +1,11 @@
+package christmas.Controller;
+
+import christmas.View.OutputView;
+
+public class Controller {
+
+
+    public static void makeIntroduce() {
+        OutputView.printIntro();
+    }
+}
---
+++ src/main/java/christmas/View/OutputView.java
@@ -0,0 +1,8 @@
+package christmas.View;
+
+public class OutputView {
+    private static final String INTRO = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static void printIntro() {
+        System.out.println(INTRO);
+    }
+}
