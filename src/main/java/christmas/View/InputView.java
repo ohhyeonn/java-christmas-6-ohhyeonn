a81d226a1e6c5caa272fb076f997059ab3a52ab4
ohhyeonn <ohhyeon120@gmail.com>
2023-11-10 02:43:26 +0900

    feat : add feature 방문날짜 입력 숫자인지 검증
    edit InputVIew.java

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -4,12 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String ERROR_VISIT_DATE_ONE_THIRTYONE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String ERROR_VISIT_DATE_IS_NUMBER = "[ERROR] 방문일자는 숫자여야 합니다.";
     public static Integer readDate() {
         int date;
         while (true) {
             try {
                 date = getVisitDate();
                 break;
+            } catch (NumberFormatException e) {
+                System.out.println(ERROR_VISIT_DATE_IS_NUMBER);
             } catch (IllegalArgumentException e) {
                 System.out.println(ERROR_VISIT_DATE_ONE_THIRTYONE);
             }
