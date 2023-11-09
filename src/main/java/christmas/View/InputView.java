ffa7009a841d3f68817c5a8194ca4a695c4799cb
ohhyeonn <ohhyeon120@gmail.com>
2023-11-10 02:38:43 +0900

    feat : add feature 방문날짜 잘못 입력시 재입력
    edit InputVIew.java

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -3,9 +3,18 @@ package christmas.View;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
+    private static final String ERROR_ONE_THIRTYONE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     public static Integer readDate() {
-        return getVisitDate();
+        int date;
+        while (true) {
+            try {
+                date = getVisitDate();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR_ONE_THIRTYONE);
+            }
+        }
+        return date;
     }
 
     private static int getVisitDate() {
