762e6d263931d089a3eab4368d9928b3db6e7e95
ohhyeonn <ohhyeon120@gmail.com>
2023-11-14 00:32:44 +0900

    feat : add test 메뉴_입력_중복_메뉴_입력_검증

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -27,7 +27,13 @@ class ApplicationTest extends NsTest {
         Menu.RED_WINE.setCount(0);
         Menu.CHAMPAGNE.setCount(0);
     }
-
+    @Test
+    void 메뉴_입력_중복_메뉴_입력_검증() {
+        assertSimpleTest(() -> {
+            runException("3" , "해산물파스타-2,해산물파스타-1");
+            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+    }
     @Test
     void 메뉴_입력_메뉴에_있는지_검증() {
         assertSimpleTest(() -> {
