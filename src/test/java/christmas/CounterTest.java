1bb691208c1063af86418123831dd90ce01eb62d
ohhyeonn <ohhyeon120@gmail.com>
2023-11-13 22:06:58 +0900

    feat : add test countBenefitsDiscountTest

--- src/test/java/christmas/CounterTest.java
+++ src/test/java/christmas/CounterTest.java
@@ -1,9 +1,12 @@
 package christmas;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.Model.Discount;
 import christmas.Model.Menu;
 import christmas.Service.Counter;
 
+import java.util.HashMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -43,8 +46,22 @@ public class CounterTest {
 
         int lumpSum = Counter.countLumpSumBeforeDiscount();
 
+        //비비큐립1개 54,000 , 아이스크림2개 10000
         assertThat(lumpSum).isEqualTo(64000);
     }
 
+    @Test
+    void countBenefitsDiscountTest(){
+        //티본스테이크 2개 = 110,000 //비비큐립 1개 = 54,000 //아이스크림 2개 = 10,000 //총 = 174,000
+        Menu.BBQ_RIBS.setCount(1);
+        Menu.ICE_CREAM.setCount(2);
+        Menu.T_BONE_STEAK.setCount(2);
+        int date = 3;
+        int lumpSumBeforeDiscount = 174000;
+        HashMap<Discount, Integer> discounts = Counter.countDiscount(date , lumpSumBeforeDiscount);
+        //크리스마스 디데이 할인: -1,200원 //평일 할인: -4,046원 //특별 할인: -1,000원 //증정 이벤트: -25,000원 //총 -31,246원
+        Integer benefitsDiscount = Counter.countBenefitsDiscount(discounts);
 
+        assertThat(benefitsDiscount).isEqualTo(-31246);
+    }
 }
