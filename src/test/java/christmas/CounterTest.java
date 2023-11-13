1720f4ee04e88136009014e1be1d20d496598054
ohhyeonn <ohhyeon120@gmail.com>
2023-11-13 22:16:45 +0900

    feat : add test countDiscountTest

--- src/test/java/christmas/CounterTest.java
+++ src/test/java/christmas/CounterTest.java
@@ -52,16 +52,37 @@ public class CounterTest {
 
     @Test
     void countBenefitsDiscountTest(){
-        //티본스테이크 2개 = 110,000 //비비큐립 1개 = 54,000 //아이스크림 2개 = 10,000 //총 = 174,000
+        //티본스테이크 2개 = 110,000 , 비비큐립 1개 = 54,000 , 아이스크림 2개 = 10,000
+        // 총 = 174,000
         Menu.BBQ_RIBS.setCount(1);
         Menu.ICE_CREAM.setCount(2);
         Menu.T_BONE_STEAK.setCount(2);
         int date = 3;
         int lumpSumBeforeDiscount = 174000;
         HashMap<Discount, Integer> discounts = Counter.countDiscount(date , lumpSumBeforeDiscount);
-        //크리스마스 디데이 할인: -1,200원 //평일 할인: -4,046원 //특별 할인: -1,000원 //증정 이벤트: -25,000원 //총 -31,246원
+
+        //크리스마스 디데이 할인: -1,200원 , 평일 할인: -4,046원 , 특별 할인: -1,000원 , 증정 이벤트: -25,000원
+        // 총 -31,246원
         Integer benefitsDiscount = Counter.countBenefitsDiscount(discounts);
 
         assertThat(benefitsDiscount).isEqualTo(-31246);
+    }
+
+    @Test
+    void countDiscountTest(){
+        //티본스테이크 2개 = 110,000 , 비비큐립 1개 = 54,000 , 아이스크림 2개 = 10,000
+        // 총 = 174,000
+        Menu.BBQ_RIBS.setCount(1);
+        Menu.ICE_CREAM.setCount(2);
+        Menu.T_BONE_STEAK.setCount(2);
+        int date = 3;
+        int lumpSumBeforeDiscount = 174000;
+
+        //크리스마스 디데이 할인: -1,200원 , 평일 할인: -4,046원 , 특별 할인: -1,000원 , 증정 이벤트: -25,000원
+        // 총 -31,246원
+        HashMap<Discount, Integer> discounts = Counter.countDiscount(date , lumpSumBeforeDiscount);
+
+        // 크리스마스 디데이 할인 -1,200원
+        assertThat(discounts.get(Discount.CHRISTMAS_DISCOUNT)).isEqualTo(-1200);
     }
 }
