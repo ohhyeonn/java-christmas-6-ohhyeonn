b3d848d5e94fdae61341c56140ff6674c4fdaffe
ohhyeonn <ohhyeon120@gmail.com>
2023-11-15 04:25:13 +0900

    refactor : separate constant
    add Constant.java

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -4,21 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.Model.Menu;
 import christmas.Model.Receipt;
 import java.util.ArrayList;
+import christmas.Constant.Constant;
 
 public class InputView {
-    private static final String ERROR_VISIT_DATE_WRONG = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    private static final String ENTER_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    private static final String ENTER_MENUS = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
-    private static final String ERROR_NOT_VALID_MENUS = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
-    private static final Integer FIRST_INDEX = 0;
-    private static final Integer SECOND_INDEX = 1;
-    private static final String REG_EXPRESS = "^[가-힣]+-[0-9]+$" ;
-    private static final Integer ZERO = 0;
-    private static final Integer THIRTYTWO = 32;
-    private static final Integer TWENTY = 20;
-    private static final String HYPHEN = "-";
-    private static final String COMMA = ",";
-    private static final String BLANC_STRING = "";
+
     private static Integer readDate() {
         int date;
         while (true) {
@@ -28,22 +17,22 @@ public class InputView {
                 validateVisitDate(date);
                 break;
             } catch (NumberFormatException e) {
-                System.out.println(ERROR_VISIT_DATE_WRONG);
+                System.out.println(Constant.ERROR_VISIT_DATE_WRONG);
             } catch (IllegalArgumentException e) {
-                System.out.println(ERROR_VISIT_DATE_WRONG);
+                System.out.println(Constant.ERROR_VISIT_DATE_WRONG);
             }
         }
         return date;
     }
 
     private static void validateVisitDate(int date) {
-        if (date <= ZERO || date >= THIRTYTWO) {
+        if (date <= Constant.ZERO || date >= Constant.THIRTYTWO) {
             throw new IllegalArgumentException();
         }
     }
 
     private static void printVisitDateInstruction() {
-        System.out.println(ENTER_VISIT_DATE);
+        System.out.println(Constant.ENTER_VISIT_DATE);
     }
 
     private static int getVisitDate() {
@@ -61,19 +50,19 @@ public class InputView {
                 validate20Menu(menusSets);
                 return menusSets;
             } catch (IllegalArgumentException e) {
-                System.out.println(ERROR_NOT_VALID_MENUS);
+                System.out.println(Constant.ERROR_NOT_VALID_MENUS);
             }
         }
 
     }
 
     private static void validate20Menu(String[] menusSets) {
-        int total = ZERO;
+        int total = Constant.ZERO;
         for(String set : menusSets){
-            String[] menuAndCount = set.split(HYPHEN);
-            total = total +Integer.parseInt(menuAndCount[SECOND_INDEX]);
+            String[] menuAndCount = set.split(Constant.HYPHEN);
+            total = total +Integer.parseInt(menuAndCount[Constant.SECOND_INDEX]);
         }
-        if(total > TWENTY){
+        if(total > Constant.TWENTY){
             throw new IllegalArgumentException();
         }
 
@@ -81,8 +70,8 @@ public class InputView {
 
     private static void validateOnlyDrinkMenu(String[] menusSets) {
         for(String set : menusSets){
-            String[] menuAndCount = set.split(HYPHEN);
-            Menu menu = Menu.getMenu(menuAndCount[FIRST_INDEX]);
+            String[] menuAndCount = set.split(Constant.HYPHEN);
+            Menu menu = Menu.getMenu(menuAndCount[Constant.FIRST_INDEX]);
             if(menu != Menu.ZERO_COLA && menu != Menu.RED_WINE && menu != Menu.CHAMPAGNE && menu != Menu.NONE){
                 return ;
             }
@@ -93,8 +82,8 @@ public class InputView {
     private static void validateDuplicatedMenu(String[] menusSets) {
         ArrayList<String> basket = new ArrayList<>();
         for(String set : menusSets){
-            String[] menuAndCount = set.split(HYPHEN);
-            String menu = menuAndCount[FIRST_INDEX];
+            String[] menuAndCount = set.split(Constant.HYPHEN);
+            String menu = menuAndCount[Constant.FIRST_INDEX];
             if(basket.contains(menu)) throw new IllegalArgumentException();
             basket.add(menu);
         }
@@ -102,12 +91,12 @@ public class InputView {
 
     private static void validateMenuSets(String[] menuSets) {
         for(String set : menuSets){
-            if(!set.matches(REG_EXPRESS)){
+            if(!set.matches(Constant.REG_EXPRESS)){
                 throw new IllegalArgumentException();
             }
-            String[] menuAndCount = set.split(HYPHEN);
-            validateMenu(menuAndCount[FIRST_INDEX]);
-            validateCount(Integer.parseInt(menuAndCount[SECOND_INDEX]));
+            String[] menuAndCount = set.split(Constant.HYPHEN);
+            validateMenu(menuAndCount[Constant.FIRST_INDEX]);
+            validateCount(Integer.parseInt(menuAndCount[Constant.SECOND_INDEX]));
         }
     }
 
@@ -119,22 +108,22 @@ public class InputView {
     }
 
     private static void validateCount(int count) {
-        if(count <= ZERO) throw new IllegalArgumentException();
+        if(count <= Constant.ZERO) throw new IllegalArgumentException();
     }
 
     private static void printMenusInstruction() {
-        System.out.println(ENTER_MENUS);
+        System.out.println(Constant.ENTER_MENUS);
     }
 
     private static String[] getMenuSets() {
         String line = Console.readLine();
         validateLine(line);
-        String[] lines = line.split(COMMA);
+        String[] lines = line.split(Constant.COMMA);
         return lines;
     }
 
     private static void validateLine(String line) {
-        if(BLANC_STRING.equals(line)) throw new IllegalArgumentException();
+        if(Constant.BLANC_STRING.equals(line)) throw new IllegalArgumentException();
     }
 
     public static Receipt makeReceipt() {
--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -1,44 +1,24 @@
 package christmas.View;
 
+import christmas.Constant.Constant;
 import christmas.Model.Badge;
 import christmas.Model.Menu;
 import christmas.Model.Receipt;
 
 public class OutputView {
-    private static final String INTRO = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    private static final String ORDERED_MENU = "<주문 메뉴>";
-    private static final String DECEMBER = "12월 ";
-    private static final String EVENT_INTRO = "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
-    private static final String BLANC = " ";
-    private static final String COUNT = "개";
-    private static final String LUMP_SUM_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
-    private static final String SUM_FORMAT = "%,d원\n";
-    private static final String GIFT_MENU = "<증정 메뉴>";
-    private static final String CHAMPAGNE = "샴페인 ";
-    private static final String NOTHING = "없음";
-    private static final String BENEFITS_LIST = "<혜택 내역>";
-    private static final String CHRISTMAS_D_DAY_DISCOUNT_FORMAT = "크리스마스 디데이 할인: %,d원\n";
-    private static final String WEEKDAY_DISCOUNT_FORMAT = "평일 할인: %,d원\n";
-    private static final String WEEKEND_DISCOUNT_FORMAT = "주말 할인: %,d원\n";
-    private static final String SPECIAL_DISCOUNT_FORMAT = "특별 할인: %,d원\n";
-    private static final String GIFT_EVENT_PRICE = "증정 이벤트: -25,000원";
-    private static final String BENEFITS_SUM_PRICE = "<총혜택 금액>";
-    private static final String ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
-    private static final String EVENT_BADGE = "<12월 이벤트 배지>";
-    private static final Integer ZERO = 0;
     public static void printIntro() {
-        System.out.println(INTRO);
+        System.out.println(Constant.INTRO);
     }
 
 
     public static void printMenu() {
         System.out.println();
-        System.out.println(ORDERED_MENU);
+        System.out.println(Constant.ORDERED_MENU);
         for(Menu menu : Menu.values()){
             int count = menu.getCount();
-            if(count != ZERO){
-                System.out.print(menu.getName()+BLANC);
-                System.out.println(count+COUNT);
+            if(count != Constant.ZERO){
+                System.out.print(menu.getName()+Constant.BLANC);
+                System.out.println(count+Constant.COUNT);
             }
 
         }
@@ -46,23 +26,23 @@ public class OutputView {
     }
 
     public static void printEventIntro(Receipt receipt) {
-        System.out.println(DECEMBER+ receipt.getVisitDate()+EVENT_INTRO );
+        System.out.println(Constant.DECEMBER+ receipt.getVisitDate()+Constant.EVENT_INTRO );
     }
 
     public static void printLumpSumBeforeDiscount(Receipt receipt) {
         System.out.println();
-        System.out.println(LUMP_SUM_BEFORE_DISCOUNT);
-        System.out.printf(SUM_FORMAT, Menu.countLumpSumBeforeDiscount());
+        System.out.println(Constant.LUMP_SUM_BEFORE_DISCOUNT);
+        System.out.printf(Constant.SUM_FORMAT, Menu.countLumpSumBeforeDiscount());
     }
 
     public static void printGiftMenu(Receipt receipt) {
         System.out.println();
-        System.out.println(GIFT_MENU);
-        if(receipt.countGiftMenu() > ZERO){
-            System.out.println(CHAMPAGNE+receipt.countGiftMenu()+COUNT);
+        System.out.println(Constant.GIFT_MENU);
+        if(receipt.countGiftMenu() > Constant.ZERO){
+            System.out.println(Constant.CHAMPAGNE+receipt.countGiftMenu()+Constant.COUNT);
             return ;
         }
-        System.out.println(NOTHING);
+        System.out.println(Constant.NOTHING);
     }
 
     public static void printBenefitsDetails(Receipt receipt) {
@@ -80,65 +60,65 @@ public class OutputView {
 
     private static void printBenefitsList() {
         System.out.println();
-        System.out.println(BENEFITS_LIST);
+        System.out.println(Constant.BENEFITS_LIST);
     }
 
     private static void printBenefitsNothing(Receipt receipt) {
-        if(receipt.countChristmasDiscount() == ZERO && receipt.countWeekDayDiscount() == ZERO && receipt.countWeekendDiscount() == ZERO && receipt.countSpecialDiscount()
-                == ZERO && receipt.countGiftMenu() == ZERO){
-            System.out.println(NOTHING);
+        if(receipt.countChristmasDiscount() == Constant.ZERO && receipt.countWeekDayDiscount() == Constant.ZERO && receipt.countWeekendDiscount() == Constant.ZERO && receipt.countSpecialDiscount()
+                == Constant.ZERO && receipt.countGiftMenu() == Constant.ZERO){
+            System.out.println(Constant.NOTHING);
         }
     }
 
     private static void printGiftEvent(Integer giftMenuCount) {
-        if(giftMenuCount > ZERO){
-            System.out.println(GIFT_EVENT_PRICE);
+        if(giftMenuCount > Constant.ZERO){
+            System.out.println(Constant.GIFT_EVENT_PRICE);
         }
     }
 
     private static void printSpecialDiscount(Integer specialDiscount) {
-        if(specialDiscount < ZERO){
-            System.out.printf(SPECIAL_DISCOUNT_FORMAT, specialDiscount);
+        if(specialDiscount < Constant.ZERO){
+            System.out.printf(Constant.SPECIAL_DISCOUNT_FORMAT, specialDiscount);
         }
     }
 
     private static void printWeekendDiscount(Integer weekendDiscount) {
-        if(weekendDiscount < ZERO){
-            System.out.printf(WEEKEND_DISCOUNT_FORMAT, weekendDiscount);
+        if(weekendDiscount < Constant.ZERO){
+            System.out.printf(Constant.WEEKEND_DISCOUNT_FORMAT, weekendDiscount);
         }
     }
 
     private static void printWeekDayDiscount(Integer weekDayDiscount) {
-        if(weekDayDiscount < ZERO){
-            System.out.printf(WEEKDAY_DISCOUNT_FORMAT, weekDayDiscount);
+        if(weekDayDiscount < Constant.ZERO){
+            System.out.printf(Constant.WEEKDAY_DISCOUNT_FORMAT, weekDayDiscount);
         }
     }
 
     private static void printChristmasDiscount(Integer christmasDiscount) {
-        if(christmasDiscount < ZERO){
-            System.out.printf(CHRISTMAS_D_DAY_DISCOUNT_FORMAT, christmasDiscount);
+        if(christmasDiscount < Constant.ZERO){
+            System.out.printf(Constant.CHRISTMAS_D_DAY_DISCOUNT_FORMAT, christmasDiscount);
         }
     }
 
     public static void printBenefitsDiscount(Receipt receipt) {
         System.out.println();
-        System.out.println(BENEFITS_SUM_PRICE);
-        if(receipt.countBenefitsDiscount() < ZERO){
-            System.out.printf(SUM_FORMAT,receipt.countBenefitsDiscount());
+        System.out.println(Constant.BENEFITS_SUM_PRICE);
+        if(receipt.countBenefitsDiscount() < Constant.ZERO){
+            System.out.printf(Constant.SUM_FORMAT,receipt.countBenefitsDiscount());
             return;
         }
-        System.out.println(NOTHING);
+        System.out.println(Constant.NOTHING);
     }
 
     public static void printEstimatedPaymentAmountAfterDiscount(Receipt receipt) {
         System.out.println();
-        System.out.println(ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT);
-        System.out.printf(SUM_FORMAT, receipt.countEstimatedPaymentAmountAfterDiscount());
+        System.out.println(Constant.ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT);
+        System.out.printf(Constant.SUM_FORMAT, receipt.countEstimatedPaymentAmountAfterDiscount());
     }
 
     public static void printBadge(Receipt receipt) {
         System.out.println();
-        System.out.println(EVENT_BADGE);
+        System.out.println(Constant.EVENT_BADGE);
         System.out.println(Badge.countBadge(receipt.countBenefitsDiscount()).getName());
     }
 }
