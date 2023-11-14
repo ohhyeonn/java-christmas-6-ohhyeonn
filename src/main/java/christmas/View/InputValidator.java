9cf88dc3146e3ccd3113cea1d60159d3db31d260
ohhyeonn <ohhyeon120@gmail.com>
2023-11-15 05:28:11 +0900

    refactor : separate validator class

---
+++ src/main/java/christmas/View/InputValidator.java
@@ -0,0 +1,74 @@
+package christmas.View;
+
+import christmas.Constant.Constant;
+import christmas.Model.Menu;
+import java.util.ArrayList;
+
+public class InputValidator {
+    public void validateVisitDate(int date) {
+        if (date <= Constant.ZERO || date >= Constant.THIRTYTWO) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateMenuSets(String[] menuSets) {
+        for(String set : menuSets){
+            if(!set.matches(Constant.REG_EXPRESS)){
+                throw new IllegalArgumentException();
+            }
+            String[] menuAndCount = set.split(Constant.HYPHEN);
+            validateMenu(menuAndCount[Constant.FIRST_INDEX]);
+            validateCount(Integer.parseInt(menuAndCount[Constant.SECOND_INDEX]));
+        }
+    }
+
+    private void validateMenu(String menu) {
+        Menu menuName = Menu.getMenu(menu);
+        if(menuName == Menu.NONE){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateCount(int count) {
+        if(count <= Constant.ZERO) throw new IllegalArgumentException();
+    }
+
+
+    public void validateDuplicatedMenu(String[] menusSets) {
+        ArrayList<String> basket = new ArrayList<>();
+        for(String set : menusSets){
+            String[] menuAndCount = set.split(Constant.HYPHEN);
+            String menu = menuAndCount[Constant.FIRST_INDEX];
+            if(basket.contains(menu)) throw new IllegalArgumentException();
+            basket.add(menu);
+        }
+    }
+
+    public void validateOnlyDrinkMenu(String[] menusSets) {
+        for(String set : menusSets){
+            String[] menuAndCount = set.split(Constant.HYPHEN);
+            Menu menu = Menu.getMenu(menuAndCount[Constant.FIRST_INDEX]);
+            if(menu != Menu.ZERO_COLA && menu != Menu.RED_WINE && menu != Menu.CHAMPAGNE && menu != Menu.NONE){
+                return ;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public void validate20Menu(String[] menusSets) {
+        int total = Constant.ZERO;
+        for(String set : menusSets){
+            String[] menuAndCount = set.split(Constant.HYPHEN);
+            total = total +Integer.parseInt(menuAndCount[Constant.SECOND_INDEX]);
+        }
+        if(total > Constant.TWENTY){
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+
+    public void validateLine(String line) {
+        if(Constant.BLANC_STRING.equals(line)) throw new IllegalArgumentException();
+    }
+}
--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -1,12 +1,11 @@
 package christmas.View;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.Model.Menu;
 import christmas.Model.Receipt;
-import java.util.ArrayList;
 import christmas.Constant.Constant;
 
 public class InputView {
+    private static InputValidator validator = new InputValidator();
 
     private static Integer readDate() {
         int date;
@@ -14,7 +13,7 @@ public class InputView {
             try {
                 printVisitDateInstruction();
                 date = getVisitDate();
-                validateVisitDate(date);
+                validator.validateVisitDate(date);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println(Constant.ERROR_VISIT_DATE_WRONG);
@@ -25,11 +24,6 @@ public class InputView {
         return date;
     }
 
-    private static void validateVisitDate(int date) {
-        if (date <= Constant.ZERO || date >= Constant.THIRTYTWO) {
-            throw new IllegalArgumentException();
-        }
-    }
 
     private static void printVisitDateInstruction() {
         System.out.println(Constant.ENTER_VISIT_DATE);
@@ -44,10 +38,10 @@ public class InputView {
             try {
                 printMenusInstruction();
                 String[] menusSets = getMenuSets();
-                validateMenuSets(menusSets);
-                validateDuplicatedMenu(menusSets);
-                validateOnlyDrinkMenu(menusSets);
-                validate20Menu(menusSets);
+                validator.validateMenuSets(menusSets);
+                validator.validateDuplicatedMenu(menusSets);
+                validator.validateOnlyDrinkMenu(menusSets);
+                validator.validate20Menu(menusSets);
                 return menusSets;
             } catch (IllegalArgumentException e) {
                 System.out.println(Constant.ERROR_NOT_VALID_MENUS);
@@ -56,60 +50,8 @@ public class InputView {
 
     }
 
-    private static void validate20Menu(String[] menusSets) {
-        int total = Constant.ZERO;
-        for(String set : menusSets){
-            String[] menuAndCount = set.split(Constant.HYPHEN);
-            total = total +Integer.parseInt(menuAndCount[Constant.SECOND_INDEX]);
-        }
-        if(total > Constant.TWENTY){
-            throw new IllegalArgumentException();
-        }
 
-    }
 
-    private static void validateOnlyDrinkMenu(String[] menusSets) {
-        for(String set : menusSets){
-            String[] menuAndCount = set.split(Constant.HYPHEN);
-            Menu menu = Menu.getMenu(menuAndCount[Constant.FIRST_INDEX]);
-            if(menu != Menu.ZERO_COLA && menu != Menu.RED_WINE && menu != Menu.CHAMPAGNE && menu != Menu.NONE){
-                return ;
-            }
-        }
-        throw new IllegalArgumentException();
-    }
-
-    private static void validateDuplicatedMenu(String[] menusSets) {
-        ArrayList<String> basket = new ArrayList<>();
-        for(String set : menusSets){
-            String[] menuAndCount = set.split(Constant.HYPHEN);
-            String menu = menuAndCount[Constant.FIRST_INDEX];
-            if(basket.contains(menu)) throw new IllegalArgumentException();
-            basket.add(menu);
-        }
-    }
-
-    private static void validateMenuSets(String[] menuSets) {
-        for(String set : menuSets){
-            if(!set.matches(Constant.REG_EXPRESS)){
-                throw new IllegalArgumentException();
-            }
-            String[] menuAndCount = set.split(Constant.HYPHEN);
-            validateMenu(menuAndCount[Constant.FIRST_INDEX]);
-            validateCount(Integer.parseInt(menuAndCount[Constant.SECOND_INDEX]));
-        }
-    }
-
-    private static void validateMenu(String menu) {
-        Menu menuName = Menu.getMenu(menu);
-        if(menuName == Menu.NONE){
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private static void validateCount(int count) {
-        if(count <= Constant.ZERO) throw new IllegalArgumentException();
-    }
 
     private static void printMenusInstruction() {
         System.out.println(Constant.ENTER_MENUS);
@@ -117,14 +59,12 @@ public class InputView {
 
     private static String[] getMenuSets() {
         String line = Console.readLine();
-        validateLine(line);
+        validator.validateLine(line);
         String[] lines = line.split(Constant.COMMA);
         return lines;
     }
 
-    private static void validateLine(String line) {
-        if(Constant.BLANC_STRING.equals(line)) throw new IllegalArgumentException();
-    }
+
 
     public static Receipt makeReceipt() {
         return new Receipt(readDate() , readMenu());
