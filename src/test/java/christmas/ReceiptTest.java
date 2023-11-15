6c15dfc9308f10d786f901f478e2bc877c9840c0
ohhyeonn <ohhyeon120@gmail.com>
2023-11-15 11:30:18 +0900

    refactor : make test to parameterizedTest
    edit ReceiptTest.java

--- src/test/java/christmas/ReceiptTest.java
+++ src/test/java/christmas/ReceiptTest.java
@@ -4,7 +4,10 @@ import christmas.Model.Badge;
 import christmas.Model.Menu;
 import christmas.Model.Receipt;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,118 +29,131 @@ public class ReceiptTest {
         Menu.RED_WINE.setCount(0);
         Menu.CHAMPAGNE.setCount(0);
     }
+    @ParameterizedTest
+    @DisplayName("방문일자 입력에 맞게 가져 오는지 테스트")
+    @CsvSource({"12,12", "29,29", "1,1"})
+    void visitDateTest(String input , String expected) {
+        Receipt receipt = new Receipt(Integer.parseInt(input), "해산물파스타-2,아이스크림-1".split(","));
 
-    @Test
-    void visitDateTest() {
-        Receipt receipt = new Receipt(12, "해산물파스타-2,아이스크림-1".split(","));
-
-        // 방문일 12 일
         int visitDate = receipt.getVisitDate();
 
-        assertThat(visitDate).isEqualTo(12);
+        assertThat(visitDate).isEqualTo(Integer.parseInt(expected));
     }
 
-    @Test
-    void lumpSumBeforeDiscountTest() {
-        Receipt receipt = new Receipt(12, "해산물파스타-2,아이스크림-2,제로콜라-2".split(","));
+    @ParameterizedTest
+    @DisplayName("할인전 주문금액 올바른 계산으로 가져오는지 테스트")
+    @CsvSource(value = {"해산물파스타-2,아이스크림-2,제로콜라-2:86000" , "티본스테이크-3,제로콜라-3:174000"},delimiter = ':')
+    void lumpSumBeforeDiscountTest(String input , String expected) {
+        Receipt receipt = new Receipt(12, input.split(","));
 
-        // 아이스크림2개 10000 , 해산물파스타2개 70000 , 제로콜라2개 6000
-        // 총 86000
+        // 할인전 주문금액
         int lumpSumBeforeDiscount = Menu.countLumpSumBeforeDiscount();
 
-        assertThat(lumpSumBeforeDiscount).isEqualTo(86000);
+        assertThat(lumpSumBeforeDiscount).isEqualTo(Integer.parseInt(expected));
     }
 
-    @Test
-    void giftMenuCountTest() {
-        Receipt receipt = new Receipt(3, "티본스테이크-2,아이스크림-2,제로콜라-2".split(","));
+    @ParameterizedTest
+    @DisplayName("증정상품 12만원 경계로 잘 가져오는지 테스트")
+    @CsvSource(value = {"아이스크림-2,티본스테이크-2:1" , "티본스테이크-2,아이스크림-1:0"},delimiter = ':')
+    void giftMenuCountTest(String input , String expected) {
+        Receipt receipt = new Receipt(3, input.split(","));
 
-        // 아이스크림2개 10000 , 티본스테이크2개 110000 , 제로콜라2개 6000
-        // 총 126000 -> 사은품 증정 1개
+        // 증정상품 갯수
         int giftMenuCount = receipt.countGiftMenu();
 
-        assertThat(giftMenuCount).isEqualTo(1);
+        assertThat(giftMenuCount).isEqualTo(Integer.parseInt(expected));
     }
 
 
-    @Test
-    void christmasDiscountTest() {
-        Receipt receipt = new Receipt(19, "크리스마스파스타-3,아이스크림-3".split(","));
+    @ParameterizedTest
+    @DisplayName("크리스마스 디데이 할인 맞게 계산하는지 테스트")
+    @CsvSource(value = {"19:-2800" , "5:-1400" , "26:0"},delimiter = ':')
+    void christmasDiscountTest(String input , String expected) {
+        Receipt receipt = new Receipt(Integer.parseInt(input), "크리스마스파스타-3,아이스크림-3".split(","));
 
-        // 12/1 1000원할인 , 12/19 2800원할인
+        // 크리스마스 할인
         int christmasDiscount = receipt.countChristmasDiscount();
 
-        assertThat(christmasDiscount).isEqualTo(-2800);
+        assertThat(christmasDiscount).isEqualTo(Integer.parseInt(expected));
     }
 
 
-    @Test
-    void weekDayDiscountTest() {
-        Receipt receipt = new Receipt(21, "바비큐립-2,레드와인-2,아이스크림-2".split(","));
+    @ParameterizedTest
+    @DisplayName("평일 할인 맞게 계산하는지 테스트")
+    @CsvSource(value = {"바비큐립-2,레드와인-2,아이스크림-2:-4046" , "해산물파스타-1,아이스크림-1:-2023" , "크리스마스파스타-1,티본스테이크-1:0"},delimiter = ':')
+    void weekDayDiscountTest(String input , String expected) {
+        Receipt receipt = new Receipt(21, input.split(","));
 
         // 12/21 목요일 (평일)
-        // 디저트메뉴 아이스크림 2개 할인 4046원
+        // 평일 할인
         int weekDayDiscount = receipt.countWeekDayDiscount();
 
-        assertThat(weekDayDiscount).isEqualTo(-4046);
+        assertThat(weekDayDiscount).isEqualTo(Integer.parseInt(expected));
     }
 
-    @Test
-    void weekendDiscountTest() {
-        Receipt receipt = new Receipt(22, "해산물파스타-1,바비큐립-1,레드와인-2,아이스크림-2".split(","));
+    @ParameterizedTest
+    @DisplayName("주말 할인 맞게 계산하는지 테스트")
+    @CsvSource(value = {"바비큐립-1,레드와인-2,아이스크림-2:-2023" , "해산물파스타-1,바비큐립-1,레드와인-2,아이스크림-2:-4046" , "아이스크림-5:0"},delimiter = ':')
+    void weekendDiscountTest(String input , String expected) {
+        Receipt receipt = new Receipt(22, input.split(","));
 
         // 12/22 금요일 주말
-        // 메인메뉴 해산물파스타 1개 바비큐립 1개 할인
+        // 주말 할인
         int weekendDiscount = receipt.countWeekendDiscount();
 
-        assertThat(weekendDiscount).isEqualTo(-4046);
+        assertThat(weekendDiscount).isEqualTo(Integer.parseInt(expected));
     }
 
 
-    @Test
-    void specialDiscountTest() {
-        Receipt receipt = new Receipt(24, "바비큐립-1,아이스크림-2".split(","));
+    @ParameterizedTest
+    @DisplayName("특별 할인 맞게 계산하는지 테스트")
+    @CsvSource(value = {"24:-1000" , "7:0"},delimiter = ':')
+    void specialDiscountTest(String input , String expected) {
+        Receipt receipt = new Receipt(Integer.parseInt(input), "바비큐립-1,아이스크림-2".split(","));
 
-        // 12/24 별표 표시 있음
-        // 1000원 할인
+        // 특별 할인
         int specialDiscount = receipt.countSpecialDiscount();
 
-        assertThat(specialDiscount).isEqualTo(-1000);
+        assertThat(specialDiscount).isEqualTo(Integer.parseInt(expected));
     }
 
 
-    @Test
-    void benefitsDiscountTest() {
-        Receipt receipt = new Receipt(18, "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1".split(","));
+    @ParameterizedTest
+    @DisplayName("총헤택 금액 맞게 계산하는지 테스트")
+    @CsvSource(value = {"티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1:-31746" , "크리스마스파스타-2,해산물파스타-1,초코케이크-1:-4723"},delimiter = ':')
+    void benefitsDiscountTest(String input , String expected) {
+        Receipt receipt = new Receipt(18, input.split(","));
 
-        // 12/18 월요일
-        // 크리스마스 디데이 할인: -2,700원
-        // 평일 할인: -4,046원
-        // 증정 이벤트: -25,000원
+        // 12/18 월요일 평
+        // 총혜택
         int benefitsDiscount = receipt.countBenefitsDiscount();
 
-        assertThat(benefitsDiscount).isEqualTo(-31746);
+        assertThat(benefitsDiscount).isEqualTo(Integer.parseInt(expected));
     }
 
 
-    @Test
-    void estimatedPaymentAmountAfterDiscountTest() {
-        Receipt receipt = new Receipt(3, "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1".split(","));
+    @ParameterizedTest
+    @DisplayName("할인후 결제예상 금액 맞게 계산하는지 테스트")
+    @CsvSource(value = {"티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1:135754" , "크리스마스파스타-2,해산물파스타-1,초코케이크-1:95777"},delimiter = ':')
+    void estimatedPaymentAmountAfterDiscountTest(String input , String expected) {
+        Receipt receipt = new Receipt(3, input.split(","));
 
         // 12/3 일요일
         int estimatedPaymentAmountAfterDiscount = receipt.countEstimatedPaymentAmountAfterDiscount();
 
-        assertThat(estimatedPaymentAmountAfterDiscount).isEqualTo(135754);
+        assertThat(estimatedPaymentAmountAfterDiscount).isEqualTo(Integer.parseInt(expected));
     }
 
 
-    @Test
-    void badgeTest() {
-        Integer benefitDiscount = -20000;
+    @ParameterizedTest
+    @DisplayName("뱃지 금액에따라 맞게 계산하는지 테스트")
+    @CsvSource(value = {"-20000:SANTA","-21000:SANTA","-19000:TREE","-10000:TREE","-9000:STAR","-5000:STAR","-4900:NONE","-500:NONE"},delimiter = ':')
+    void badgeTest(String input , String expected) {
+        Integer benefitDiscount = Integer.parseInt(input);
 
         // 20000원 이상 산타
         Badge badge = Badge.countBadge(benefitDiscount);
 
-        assertThat(badge).isEqualTo(Badge.SANTA);
+        assertThat(badge).isEqualTo(Badge.valueOf(expected));
     }
 }
